44c4f355c937cdcd9e896d0e481d56b817d2fb42
Peter Borkuti <peter@aspire>
2017-12-25 22:52:46 +0100

    new module - inlude-param

---
+++ modules/include-param/src/main/java/include/param/constants/IncludeParamPortletKeys.java
@@ -0,0 +1,10 @@
+package include.param.constants;
+
+/**
+ * @author peter
+ */
+public class IncludeParamPortletKeys {
+
+	public static final String IncludeParam = "IncludeParam";
+
+}
---
+++ modules/include-param/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="include-param.caption"/></b>
+</p>
