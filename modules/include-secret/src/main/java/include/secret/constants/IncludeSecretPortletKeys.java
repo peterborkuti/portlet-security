6e0f88cd169c972bb7e8928185a8fd65af764dba
Peter Borkuti <peter@aspire>
2017-12-26 16:28:48 +0100

    include-secret initial commit

---
+++ modules/include-secret/src/main/java/include/secret/constants/IncludeSecretPortletKeys.java
@@ -0,0 +1,10 @@
+package include.secret.constants;
+
+/**
+ * @author peter
+ */
+public class IncludeSecretPortletKeys {
+
+	public static final String IncludeSecret = "IncludeSecret";
+
+}
---
+++ modules/include-secret/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="include-secret.caption"/></b>
+</p>
