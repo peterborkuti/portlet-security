6c01bafcff7ccaecd2f2b9fe559a5942385783f2
Peter Borkuti <peter@aspire>
2017-12-26 22:03:04 +0100

    persistent-xss initial commit

---
+++ modules/persistent-xss/src/main/java/persistent/xss/constants/PersistentXSSPortletKeys.java
@@ -0,0 +1,10 @@
+package persistent.xss.constants;
+
+/**
+ * @author peter
+ */
+public class PersistentXSSPortletKeys {
+
+	public static final String PersistentXSS = "PersistentXSS";
+
+}
---
+++ modules/persistent-xss/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="persistent-xss.caption"/></b>
+</p>
