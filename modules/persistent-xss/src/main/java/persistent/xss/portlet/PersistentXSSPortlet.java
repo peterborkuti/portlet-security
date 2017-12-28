904dbabdb63d86fa540a2c8d13c6795fdd792d10
Peter Borkuti <peter@aspire>
2017-12-28 08:16:36 +0100

    persistent xss

--- modules/persistent-xss/src/main/java/persistent/xss/portlet/PersistentXSSPortlet.java
+++ modules/persistent-xss/src/main/java/persistent/xss/portlet/PersistentXSSPortlet.java
@@ -1,12 +1,22 @@
 package persistent.xss.portlet;
 
-import persistent.xss.constants.PersistentXSSPortletKeys;
+import java.io.IOException;
 
-import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletPreferences;
+import javax.portlet.ReadOnlyException;
+import javax.portlet.ValidatorException;
 
 import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import persistent.xss.constants.PersistentXSSPortletKeys;
 
 /**
  * @author peter
@@ -26,4 +36,32 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class PersistentXSSPortlet extends MVCPortlet {
+
+	public void addEntry(ActionRequest request, ActionResponse response) {
+		storePreferences(setMessage(request, ParamUtil.getString(request, "message")));
+	}
+
+	private PortletPreferences setMessage(ActionRequest request, String entry){
+		PortletPreferences prefs = request.getPreferences();
+
+		String message = ParamUtil.getString(request, "message");
+
+		try {
+			prefs.setValue("message", message);
+		} catch (ReadOnlyException e) {
+			_log.error(e);
+		}
+
+		return prefs;
+	}
+
+	private void storePreferences(PortletPreferences prefs) {
+		try {
+			prefs.store();
+		} catch (ValidatorException | IOException e) {
+			_log.error(e);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(PersistentXSSPortlet.class.getName());
 }
--- modules/persistent-xss/src/main/resources/META-INF/resources/view.jsp
+++ modules/persistent-xss/src/main/resources/META-INF/resources/view.jsp
@@ -1,5 +1,21 @@
 <%@ include file="/init.jsp" %>
 
+<portlet:actionURL name="addEntry" var="addEntryURL"></portlet:actionURL>
+
+<aui:form action="<%= addEntryURL %>" name="<portlet:namespace />fm" method="post">
+	<aui:fieldset>
+		<aui:input name="message"></aui:input>
+	</aui:fieldset>
+
+	<aui:button-row>
+		<aui:button type="submit"></aui:button>
+	</aui:button-row>
+</aui:form>
+
+<%
+String message = portletPreferences.getValue("message", "No message");
+%>
+
 <p>
-	<b><liferay-ui:message key="persistent-xss.caption"/></b>
+	<b>Message:<%= message %></b>
 </p>
